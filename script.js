47e4cf08c6235c9acb66e91aa11c1cfd437304c7
Bhavishay Chawla <bhavishay97@outlook.com>
2020-06-29 16:25:04 +0530

    added generate random background button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const htmlBody = document.body;
 const color1 = document.querySelector('.color1');
 const color2 = document.querySelector('.color2');
 const currentSelection = document.querySelector('.current-bg');
+const randomButton = document.querySelector('.random-btn');
 
 function setGradient() {
   htmlBody.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
@@ -9,6 +10,17 @@ function setGradient() {
   currentSelection.textContent = `${htmlBody.style.background};`;
 }
 
+function generateRandom() {
+  const randomColor1 = Math.random().toString(16).slice(2, 8).toUpperCase();
+  const randomColor2 = Math.random().toString(16).slice(2, 8).toUpperCase();
+
+  color1.value = `#${randomColor1}`;
+  color2.value = `#${randomColor2}`;
+
+  setGradient();
+}
+
 window.addEventListener('load', setGradient);
 color1.addEventListener('input', setGradient);
 color2.addEventListener('input', setGradient);
+randomButton.addEventListener('click', generateRandom);
